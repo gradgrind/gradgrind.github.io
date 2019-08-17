cf166611641d58dce1d434a40d11af2268e73ee4
Michael Towers <mt.bothfeld@gmail.com>
2019-08-17 18:43:12 +0200

    Add update script.

--- update.sh
+++ update.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 ( cd src; mkdocs build )
-rsync -avcn src/site/ . --exclude=.* --exclude=src --exclude=README.md --exclude=update.sh --delete
-#rsync -avc src/site/ . --exclude=.* --exclude=src --exclude=README.md --exclude=update.sh --delete
+
+rsync -avcn src/site/ . --exclude=.* --exclude=src --exclude=sitemap.* --exclude=README.md --exclude=update.sh --delete
+
+read -p 'Continue?: [y for yes] ' ok
+[[ "$ok" != y* ]] && exit 1
+
+rsync -avc src/site/ . --exclude=.* --exclude=src --exclude=sitemap.* --exclude=README.md --exclude=update.sh --delete
+
 git add -A && git commit -m "Update pages"
+
+read -p 'Push to github?: [y for yes] ' ok
+[[ "$ok" != y* ]] && exit 1
+
 git push origin master
 
